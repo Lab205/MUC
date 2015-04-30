7895d3aec9ddc81a732487bf2a5f66e933b3b640
Saffie <saffiechu@gmail.com>
2015-04-30 16:52:59 +0800

    初始化工程结构

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: user
+ *
+ * Created on 2015年4月30日, 下午4:47
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
